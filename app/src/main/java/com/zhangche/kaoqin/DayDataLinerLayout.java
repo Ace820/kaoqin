3c11c1d290fba02b6f2759e855ede78a2d5a475e
zhangche <zhangche@baidu.com>
2019-06-24 20:03:41 +0800

    Fix main function(less for data save)

--- app/src/main/java/com/zhangche/kaoqin/DayDataLinerLayout.java
+++ app/src/main/java/com/zhangche/kaoqin/DayDataLinerLayout.java
@@ -1,13 +1,17 @@
 package com.zhangche.kaoqin;
 
+import android.app.AlertDialog;
+import android.app.TimePickerDialog;
 import android.content.Context;
 import android.graphics.Color;
 import android.graphics.drawable.Drawable;
 import android.view.Gravity;
+import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.TimePicker;
 
 import androidx.core.content.res.ResourcesCompat;
 
@@ -19,9 +23,11 @@ import java.util.ResourceBundle;
 
 public class DayDataLinerLayout extends LinearLayout {
 
+    Context mContext;
     TextView date,workOn,workOff;
-    public DayDataLinerLayout(Context context, int time, Date workOnTime, Date workOffTime) {
+    public DayDataLinerLayout(Context context, String time, Date workOnTime, Date workOffTime) {
         super(context);
+        mContext = context;
         date = new TextView(context);
         workOn = new TextView(context);
         workOff = new TextView(context);
@@ -29,8 +35,8 @@ public class DayDataLinerLayout extends LinearLayout {
 
         setOrientation(HORIZONTAL);
 
-        setShowDividers(SHOW_DIVIDER_MIDDLE);
-        setDividerDrawable(ResourcesCompat.getDrawable(getResources(),R.drawable.black_line,null));
+//        setShowDividers(SHOW_DIVIDER_MIDDLE);
+//        setDividerDrawable(ResourcesCompat.getDrawable(getResources(),R.drawable.black_line,null));
         addView(date);
         addView(workOn);
         addView(textView);
@@ -40,8 +46,8 @@ public class DayDataLinerLayout extends LinearLayout {
         dateLayoutParams.weight = 2;
         dateLayoutParams.height = ViewGroup.LayoutParams.MATCH_PARENT;
         date.setLayoutParams(dateLayoutParams);
-        date.setText(String.valueOf(time));
-        date.setGravity(Gravity.CENTER);
+        date.setText(getWeekDays(time));
+        date.setGravity(Gravity.CENTER_VERTICAL|Gravity.RIGHT);
 //        date.setBackgroundColor(Color.WHITE);
 
 
@@ -50,6 +56,7 @@ public class DayDataLinerLayout extends LinearLayout {
         workOnLayoutParams.height = ViewGroup.LayoutParams.MATCH_PARENT;
         workOn.setLayoutParams(workOnLayoutParams);
         workOn.setText(getFormatTime(workOnTime,true));
+        workOn.setOnClickListener(new dateClickListener(false));
         workOn.setGravity(Gravity.CENTER);
 //        workOn.setBackgroundColor(Color.WHITE);
 
@@ -59,24 +66,63 @@ public class DayDataLinerLayout extends LinearLayout {
         textView.setLayoutParams(textLayoutParams);
         textView.setText("-");
         textView.setTextSize(30);
-        textView.setGravity(Gravity.CENTER);
+        textView.setGravity(Gravity.CENTER_VERTICAL|Gravity.LEFT);
 
         LinearLayout.LayoutParams workOffLayoutParams = (LayoutParams) workOff.getLayoutParams();
         workOffLayoutParams.weight = 2;
         workOffLayoutParams.height = ViewGroup.LayoutParams.MATCH_PARENT;
         workOff.setLayoutParams(workOffLayoutParams);
         workOff.setText(getFormatTime(workOffTime,false));
-        workOff.setGravity(Gravity.CENTER);
+        workOff.setOnClickListener(new dateClickListener(true));
+        workOff.setGravity(Gravity.CENTER_VERTICAL|Gravity.LEFT);
 //        workOff.setBackgroundColor(Color.WHITE);
 
     }
 
+    class dateClickListener implements View.OnClickListener {
+        private int hourOfDay = 8;
+        private int minuteOfHour = 30;
+        boolean isWorkOff = false;
+        public dateClickListener(boolean workOff) {
+            if (workOff) {
+                hourOfDay = 17;
+                isWorkOff = true;
+            }
+        }
+        @Override
+        public void onClick(View v) {
+            new TimePickerDialog(mContext, AlertDialog.THEME_HOLO_LIGHT, new TimePickerDialog.OnTimeSetListener() {
+                @Override
+                public void onTimeSet(TimePicker timePicker, int hour, int minute) {
+                    if (isWorkOff)
+                        workOff.setText(hour + ":" + minute);
+                    else
+                        workOn.setText(hour + ":" + minute);
+                }
+            },hourOfDay,minuteOfHour,true).show();
+
+        }
+    }
     private String getFormatTime(Date time,boolean isMorning) {
         if (time == null)
-            return isMorning ? "8:30" : "?";
+            return isMorning ? "8:30" : "??:??";
         Calendar calendar = Calendar.getInstance(Locale.CHINA);
         SimpleDateFormat sdf = new SimpleDateFormat("hh:mm", Locale.CHINA);
         sdf.format(calendar.getTime());
         return sdf.format(calendar.getTime());
+    }
+    private String getWeekDays(String date) {
+        String[] weekDayName = {"日","一","二","三","四","五","六"};
+        Calendar calendar = Calendar.getInstance(Locale.CHINA);
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd", Locale.CHINA);
+        try {
+            calendar.setTime(sdf.parse(date));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        int dayOfWeek = calendar.get(Calendar.DAY_OF_WEEK) -1;
+        int dayOfMonth = calendar.get(Calendar.DAY_OF_MONTH);
+        return dayOfMonth + "  (" + weekDayName[dayOfWeek] + ")";
+
     }
 }
--- app/src/main/java/com/zhangche/kaoqin/MainActivity.java
+++ app/src/main/java/com/zhangche/kaoqin/MainActivity.java
@@ -2,25 +2,153 @@ package com.zhangche.kaoqin;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.app.DatePickerDialog;
+import android.content.DialogInterface;
 import android.graphics.drawable.GradientDrawable;
 import android.os.Bundle;
 import android.text.Layout;
+import android.util.Log;
 import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
+import java.util.regex.Matcher;
+
 public class MainActivity extends AppCompatActivity {
 
+    int mYear = 2019;
+    int mMonth = 6;
+    int mDay = 20;
+    int tempYear = mYear;
+    int tempMonth = mMonth;
+    TextView monthInfo;
+    LinearLayout mainLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        LinearLayout mainLayout = findViewById(R.id.main_layout);
+        mainLayout = findViewById(R.id.main_layout);
 
+        monthInfo = findViewById(R.id.date_info);
+        monthInfo.setText(getMonth(null));
+        monthInfo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                createMonthChooser();
+            }
+        });
 
-        for(int i = 1;i<31;i++) {
-            mainLayout.addView(new DayDataLinerLayout(MainActivity.this,i,null,null));
+        reDrawViews();
+    }
+
+    private void reDrawViews() {
+        monthInfo.setText(mYear + "年" + mMonth + "月");
+        mainLayout.removeAllViews();
+        for(int i = 0;i<getMonthDays(mYear,mMonth);i++) {
+            mainLayout.addView(new DayDataLinerLayout(MainActivity.this,formatTime(mYear,mMonth,i + 1),null,null));
         }
+
+    }
+    private void createMonthChooser() {
+        final AlertDialog.Builder alertDialog = new AlertDialog.Builder(MainActivity.this);
+        alertDialog.setIcon(null);
+        alertDialog.setTitle("选择月份");
+        alertDialog.setCancelable(true);
+        LayoutInflater inflater = getLayoutInflater();
+        View layout = inflater.inflate(R.layout.month_chooser,(ViewGroup) findViewById(R.id.month_chooser));
+        DatePicker dp = layout.findViewById(R.id.datePicker);
+        dp.init(mYear, mMonth - 1, mDay, new dateChangeListener());
+        ((ViewGroup)((ViewGroup) dp.getChildAt(0)).getChildAt(0)).getChildAt(2).setVisibility(View.GONE);
+        alertDialog.setView(layout);
+        alertDialog.setPositiveButton("确定",
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        //...To-do
+                        mYear = tempYear;
+                        mMonth = tempMonth;
+                        reDrawViews();
+                    }
+                });
+            alertDialog.setNegativeButton("取消",
+                    new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            //...To-do
+                            tempYear = mYear;
+                            tempMonth = mMonth;
+                        }
+                    });
+        // 显示
+        alertDialog.show();
+    }
+
+    class dateChangeListener implements DatePicker.OnDateChangedListener {
+        @Override
+        public void onDateChanged(DatePicker datePicker, int year, int month, int day) {
+            tempYear = year;
+            tempMonth = month + 1;
+        }
+    }
+    private String getMonth(String targetDate) {
+        Calendar calendar = Calendar.getInstance(Locale.CHINA);
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy年MM月", Locale.CHINA);
+        try {
+            if (targetDate != null)
+                calendar.setTime(sdf.parse(targetDate));
+            sdf.format(calendar.getTime());
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
+        return sdf.format(calendar.getTime());
+
+    }
+    public static int getMonthDays(int year, int month) {
+        switch (month) {
+            case 1:
+            case 3:
+            case 5:
+            case 7:
+            case 8:
+            case 10:
+            case 12:
+                return 31;
+            case 4:
+            case 6:
+            case 9:
+            case 11:
+                return 30;
+            case 2:
+                if (((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0)){
+                    return 29;
+                }else{
+                    return 28;
+                }
+            default:
+                return -1;
+        }
+    }
+    private String formatTime(int year, int month, int day) {
+        String result = "";
+        if (month < 10)
+            result = result + year + "0" + month;
+        else
+            result = result + year + month;
+
+        if (day < 10)
+            result = result + "0" + day;
+        else
+            result = result + day;
+
+        return result;
     }
 }
